82797c10022f1b6d1fa5f300101a59c62a434ede
kebin <2356995865@qq.com>
2020-05-08 20:43:37 +0800

    首页提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,14 @@ const app = getApp()
 
 Page({
   data: {
-
+    headerIconShow: false
   },
   onLoad: function () {
     
-  }
+  },
+  toSeeMoreIcon () {
+    this.setData({
+      headerIconShow: !this.data.headerIconShow
+    })
+  },
 })

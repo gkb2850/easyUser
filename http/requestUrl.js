202197dcfb16435ac310b0d0b67e99ba2a871cc9
kebin <2356995865@qq.com>
2020-06-17 18:54:45 +0800

    提交

--- http/requestUrl.js
+++ http/requestUrl.js
@@ -1,11 +1,12 @@
-const requestUrl = (url, data= {}, method) => {
+const requestUrl = (url, data = {}, method) => {
   return new Promise((resolve, reject) => {
     let htype = url.indexOf('vop.baidu.com/server_api') > -1 ? 1 : 0
+    console.log(JSON.stringify(data))
     wx.request({
       url: url,
       header: {
-        // 'content-type': htype === 1 ? 'application/json' : 'application/x-www-form-urlencoded'
-        'content-type': 'application/jason;charset=utf-8'
+        'content-type': htype === 1 ? 'application/json' : 'application/x-www-form-urlencoded'
+        // 'content-type':'application/json'
       },
       data: JSON.stringify(data),
       method: method,
